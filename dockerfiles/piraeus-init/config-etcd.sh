7027af93be05ad4ada6e636ec789923430b2ece9
alexzhc <alexzhc@hotmail.com>
2020-01-03 14:08:10 +0800

    return to a simplified fixed 3-node etcd deployment for PoC

--- dockerfiles/piraeus-init/config-etcd.sh
+++ dockerfiles/piraeus-init/config-etcd.sh
@@ -1,56 +1,34 @@
 #!/bin/bash -ex
 source /init/cmd/tools.sh
 
-# identify network mode, and get ip and address
-ETCD_IP=${THIS_POD_IP}
-if [[ "${THIS_NODE_IP}" == "${THIS_POD_IP}" ]]; then
-    echo Found host network, using host ip
-    ETCD_ADDR=${ETCD_IP}
-elif [[ "${HOSTNAME}" == "${THIS_POD_NAME}" ]]; then
-    echo Found kube network, using pod dns name
-    #THIS_STS_NAME=$( echo ${THIS_POD_NAME} | sed 's/-[0-9]*$//' )
-    ETCD_ADDR="${THIS_POD_NAME}.${THIS_POD_NAME/-[0-9]*/}.${THIS_POD_NAMESPACE}"   
-else
-    echo Unable to identify network mode
-    exit 1
-fi
+# assemble etcd urls
+THIS_POD_SET=${THIS_POD_NAME/-[0-9]*/}
 
-# set internal ports to svc ports     
-PEER_PORT=${PIRAEUS_ETCD_SERVICE_PORT_PEER}
-CLIENT_PORT=${PIRAEUS_ETCD_SERVICE_PORT_CLIENT}
-export ETCDCTL_ENDPOINTS="http://${PIRAEUS_ETCD_SERVICE_HOST}:${CLIENT_PORT}"
+SVC_PEER_PORT_VAR="${THIS_POD_SET^^}_SERVICE_PORT_PEER"
+SVC_PEER_PORT_VAR=${SVC_PEER_PORT_VAR/-/_}
+PEER_PORT=${!SVC_PEER_PORT_VAR}
 
-# add to cluster and 
-if [ ! -f /init/conf/etcd.conf ]; then
-    echo Adding node to the cluster
-    if [[ ${THIS_POD_NAME} =~ -etcd-0$ ]] && ! _best-effort etcdctl endpoint status; then
-        INI_STATE=new
-    elif _best-effort etcdctl member list; then 
-        INI_STATE=existing
-        EXISTING_CLUSTER=$( cat /init/.best_effort_ouput | grep -v ${ETCD_ADDR} | sed 's/,//g' |  awk ' /-etcd-[0-9]+/ {print ","$3"="$4}' )
-        _best-effort etcdctl member add etcd-${ETCD_ADDR} --peer-urls=http://${ETCD_ADDR}:${PEER_PORT}
-    else
-        exit 1
-    fi
+SVC_CLIENT_PORT_VAR="${THIS_POD_SET^^}_SERVICE_PORT_CLIENT"
+SVC_CLIENT_PORT_VAR=${SVC_CLIENT_PORT_VAR/-/_}
+CLIENT_PORT=${!SVC_CLIENT_PORT_VAR}
 
-    # write config file
-    cat > /init/conf/etcd.conf <<EOF
-name: ${THIS_POD_NAME}
-max-txn-ops: 1024
-listen-peer-urls: http://${ETCD_IP}:${PEER_PORT}
-listen-client-urls: http://${ETCD_IP}:${CLIENT_PORT}
-advertise-client-urls: http://${ETCD_ADDR}:${CLIENT_PORT}
-initial-advertise-peer-urls: http://${ETCD_ADDR}:${PEER_PORT}
-initial-cluster-token: piraeus-etcd-cluster
-initial-cluster: "${THIS_POD_NAME}=http://${ETCD_ADDR}:${PEER_PORT}${EXISTING_CLUSTER}"
-initial-cluster-state: ${INI_STATE}
-data-dir: /var/run/etcd/data
-enable-v2: true
+ETCD_CLUSTER="$( seq 0 2 | xargs -tI % echo "${THIS_POD_SET}-%=http://${THIS_POD_SET}-%.${THIS_POD_SET}:${PEER_PORT}" | paste -sd "," - )"
+
+# write config file
+cat > /init/conf/etcd.conf <<EOF
+name:                        ${THIS_POD_NAME}
+max-txn-ops:                 1024
+listen-peer-urls:            http://${THIS_POD_IP}:${PEER_PORT}
+listen-client-urls:          http://${THIS_POD_IP}:${CLIENT_PORT},http://127.0.0.1:${CLIENT_PORT}
+advertise-client-urls:       http://${THIS_POD_NAME}.${THIS_POD_SET}:${CLIENT_PORT}
+initial-advertise-peer-urls: http://${THIS_POD_NAME}.${THIS_POD_SET}:${PEER_PORT}
+initial-cluster-token:       ${THIS_POD_SET}
+initial-cluster:             ${ETCD_CLUSTER}
+initial-cluster-state:       new
+data-dir:                    /.etcd/data
+enable-v2:                   true
 EOF
-fi
-
-# check config file
 cat /init/conf/etcd.conf
 
-# get local endpoints
-echo http://${ETCD_IP}:${CLIENT_PORT} > /init/conf/etcd_local_endpoint
+# check resolv.conf
+cat /etc/resolv.conf
